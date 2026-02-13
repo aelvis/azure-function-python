0c0f5730f4f9bcc84270340439e929dbd1613a94
Elvis Alcántara <elvis_peke@hotmail.com>
2026-02-13 10:00:41 -0500

    Update
    update

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="pandas")
 def pandas(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed a request..')
 
     # Obtener archivo enviado como form-data
     file = req.files.get('file')
